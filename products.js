6c3e63387182dc71d24f19f1ffd37e61da9f4d4d
magiccandles <aty.munca@gmail.com>
2026-01-07 13:52:26 +0200

    Update products.js

--- products.js
+++ products.js
@@ -4,6 +4,7 @@ const products = [
         name: "Lumânare flori",
         price: 20,
         image: "c1.jpg",
+        image2: "c1_alt.jpg",
         desc: "O lumânare delicată cu motive florale, turnată manual."
     },
     {
@@ -11,6 +12,7 @@ const products = [
         name: "Odorizant Trandafir",
         price: 15,
         image: "c2.jpg",
+        image2: "c2_alt.jpg",
         desc: "Parfumul clasic și elegant al trandafirilor englezești."
     },
     {
@@ -18,6 +20,7 @@ const products = [
         name: "Vanilie caldă",
         price: 5,
         image: "poze/vanilie.png",
+        image2: "poze/vanilie_2.png",
         desc: "O armoă fină de vanilie."
     },
     {
@@ -25,6 +28,7 @@ const products = [
         name: "Migdale & Ciocolată albă",
         price: 5,
         image: "poze/migdale_choco_alba.png", 
+        image2: "poze/migdale_choco_alba_2.png",
         desc: "Relaxare totală cu note profunde de migdale și ciocolată albă."
     },
     {
@@ -32,6 +36,7 @@ const products = [
         name: "Vanilie & Lămâie",
         price: 5,
         image: "poze/vanilie_lamaie.png", 
+        image2: "poze/vanilie_lamaie_2.png",
         desc: "Ceară parfumată cu arome de vanilie și lămâie."
     },
     {
@@ -39,6 +44,7 @@ const products = [
         name: "Portocale Coapte",
         price: 5,
         image: "poze/portocale.png", 
+        image2: "poze/portocale_2.png",
         desc: "Ceară parfumată cu aromă de portocală coaptă."
     },
     {
@@ -46,6 +52,7 @@ const products = [
         name: "Iarna ca-n povești",
         price: 5,
         image: "poze/iarna.png", 
+        image2: "poze/iarna_2.png",
         desc: "Ceară parfumată cu arome de iarnă."
     },
     {
@@ -53,6 +60,7 @@ const products = [
         name: "Magia Craciunului",
         price: 5,
         image: "poze/magia_craciunului.png", 
+        image2: "poze/magia_craciunului_2.png",
         desc: "Lumanare festivă pentru momente magice."
     },
     {
@@ -60,6 +68,7 @@ const products = [
         name: "Fistic Sarat",
         price: 5,
         image: "poze/fistic_sarat.png", 
+        image2: "poze/fistic_sarat_2.png",
         desc: "O combinatie surprinzătoare de fistic cu sare."
     },
     {
@@ -67,6 +76,7 @@ const products = [
         name: "Muguri de pin si Vanilie",
         price: 5,
         image: "poze/pin_vanilie.png", 
+        image2: "poze/pin_vanilie_2.png",
         desc: "Mireasma craciunului, muguri de pin si vanilie."
     },
     {
@@ -74,6 +84,7 @@ const products = [
         name: "Clementine dulci",
         price: 5,
         image: "poze/clementine.png", 
+        image2: "poze/clementine_2.png",
         desc: "O aroma inimaginabila de clementine dulci."
     },
     {
@@ -81,6 +92,7 @@ const products = [
         name: "Strudel cu mere",
         price: 5,
         image: "poze/strudel_mere.png", 
+        image2: "poze/strudel_mere_2.png",
         desc: "Un miros ce iti aduce aminte de strudelele copilariei"
     },
     {
@@ -88,6 +100,7 @@ const products = [
         name: "Ciocolată & Portocale",
         price: 5,
         image: "poze/cioco_porto.png",
+        image2: "poze/cioco_porto_2.png",
         desc: "Dulce și primitoare."
     }
 ];
