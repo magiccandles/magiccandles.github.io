1568e0da43773c42d26d75f32fda23065bc656d1
magiccandles <aty.munca@gmail.com>
2026-01-07 12:21:17 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: 1,
+        name: "Lumânare flori",
+        price: 45,
+        image: "c1.jpg",
+        desc: "O lumânare delicată cu motive florale, turnată manual."
+    },
+    {
+        id: 2,
+        name: "Odorizant Trandafir",
+        price: 20,
+        image: "c2.jpg",
+        desc: "Parfumul clasic și elegant al trandafirilor englezești."
+    },
+    {
+        id: 3,
+        name: "Pepene & Castravete",
+        price: 20,
+        image: "c3.jpg",
+        desc: "O combinație fresh, ideală pentru a împrospăta aerul."
+    },
+    {
+        id: 4,
+        name: "Lavandă & Mosc",
+        price: 35,
+        image: "c1.jpg", 
+        desc: "Relaxare totală cu note profunde de mosc."
+    },
+    {
+        id: 5,
+        name: "Vanilie",
+        price: 30,
+        image: "c2.jpg",
+        desc: "Dulce și primitoare."
+    }
+];
